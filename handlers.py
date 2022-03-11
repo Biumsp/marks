12302d2f616d04072eda76dc7f1e803e08205c64
Enrico Bussetti <78282465+Biumsp@users.noreply.github.com>
2022-03-11 21:55:16 +0100

    Remove useless function from script

--- handlers.py
+++ handlers.py
@@ -8,22 +8,9 @@ from telegram_user import Telegram_User
 
 GROUP_HELP  = "Group Help Message" 
 
-
-def is_private_chat(update: Update):
-    return int(update.effective_chat.id) > 0
-
-
 def start_help(update: Update, context: CallbackContext):
 
     chat_id = update.effective_chat.id
-
-    if is_private_chat(update):
-        return
-    else:
-        # context.bot.sendMessage(chat_id=chat_id,
-        #                         text=GROUP_HELP,
-        #                         parse_mode=ParseMode.HTML)
-
-        context.bot.sendMessage(chat_id=chat_id,
-                                 text=update,
-                                 parse_mode=ParseMode.HTML)
+    context.bot.sendMessage(chat_id=chat_id,
+                            text=update,
+                            parse_mode=ParseMode.HTML)
